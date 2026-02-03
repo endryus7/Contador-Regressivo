b39c39ae371cb6787a19365cfcac3f90a1d62d84
Endryus-Montezano <endryusmontezano@live.com>
2026-02-03 16:53:39 -0300

    Cria componentes principais

---
+++ Contador_Regressivo/src/App.jsx
@@ -0,0 +1,26 @@
+import Title from './components/Title'
+import Counter from './components/Counter'
+
+import NewYear from "./assets/newyear.png"
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className='App' style={{backgroundImage: `url(${NewYear})`}}>
+      <div className="container">
+        <Title title="Contagem regressiva para 2026" />
+        <div className="countdown-container">
+          <Counter title="Dias" number={2} />
+          <Counter title="Horas" number={2} />
+          <Counter title="Minutos" number={2} />
+          <Counter title="Segundos" number={2} />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
